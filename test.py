1f01add08c615f2a081875efd3c0635205aed4d6
Regina Sorokina <Reginkaaa@mail.ru>
2022-07-15 21:25:39 +0300

    'supertest'

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ first_user_name = input("Введите ваше имя")
 user_mail = input("Введите свой e-mail: ")
 print(user_mail)
 # проверка создания ветки
-print("Тест для создания корзыны")
+print("Тест для ")
